18b540e30773070fb9d746119f0b0e2ee4e855b1
Anna Rooks <a.Git@ironflurry.com>
2017-03-08 22:17:36 -0500

    Added replace method for emoji with prefixed names (for selfbot mode)

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const replace = require("./replace.json");
 const config = require("./config.json");
 const cmds = require("./commands.json");
 
@@ -8,7 +9,7 @@ client.login(config.token);
 
 client.on('ready', () => {
     client.user.setGame("with fire");
-    console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+    console.log(`Ready to activate in ${client.channels.size} channels on ${client.guilds.size} servers, against a total of ${client.users.size} users.`);
 });
 
 client.on("message", (message) => {
@@ -17,6 +18,20 @@ client.on("message", (message) => {
     // Must be prepended with the prefix and not a bot (for using with bot account)
     if (message.author.id !== config.ownerID) return;
     if (message.author.bot) return;
+
+    if (message.content.match(/&\w+/g)) {
+        let toReplace = message.content.match(/&\w+/g)
+            .map(i => {return i.substring(replace.prefix.length)})
+            .filter(i => {return !!replace.matches[i]});
+        console.log(`[emoji] replacing ${toReplace}!`);
+        let modifiedMessage = message.content;
+        for (var i = 0; i < toReplace.length; i++) {
+            let r = toReplace[i];
+            modifiedMessage = modifiedMessage.replace(replace.prefix + r, replace.matches[r]);
+        }
+        message.reply(modifiedMessage);
+    }
+
     if (!message.content.startsWith(config.prefix)) return;
     let cmd = message.content.substring(1).trim();
 
