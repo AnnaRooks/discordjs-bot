8047ed417aa9ad1b74d900d062766381d1a92901
Anna Rooks <a.Git@ironflurry.com>
2017-03-13 04:43:03 -0400

    Restructured bot to be easier to set use mode

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+"use strict";
+var argv = require('minimist')(process.argv.slice(2));
+
 const Discord = require("discord.js");
 const replace = require("./replace.json");
 const config = require("./config.json");
@@ -8,35 +11,38 @@ const client = new Discord.Client();
 client.login(config.token);
 
 client.on('ready', () => {
-    client.user.setGame("with fire");
+    client.user.setGame(argv.game);
     console.log(`Ready to activate in ${client.channels.size} channels on ${client.guilds.size} servers, against a total of ${client.users.size} users.`);
 });
 
 client.on("message", (message) => {
-    // Must be message from self (for using with selfbot)
-    // if (message.author.id !== client.user.id) return;
-    // Must be prepended with the prefix and not a bot (for using with bot account)
-    if (message.author.id !== config.ownerID) return;
-    if (message.author.bot) return;
+    if (argv.selfbot) {
+        // Must be message from self (for using with selfbot)
+        if (message.author.id !== client.user.id) return;
 
-    if (message.content.match(/&\w+/g)) {
-        let toReplace = message.content.match(/&\w+/g)
-            .map(i => {return i.substring(replace.prefix.length)})
-            .filter(i => {return !!replace.matches[i]});
-        console.log(`[emoji] replacing ${toReplace}!`);
-        let modifiedMessage = message.content;
-        for (var i = 0; i < toReplace.length; i++) {
-            let r = toReplace[i];
-            modifiedMessage = modifiedMessage.replace(replace.prefix + r, replace.matches[r]);
+        if (message.content.match(/&\w+/g)) {
+            let toReplace = message.content.match(/&\w+/g)
+                .map(i => {return i.substring(replace.prefix.length)})
+                .filter(i => {return !!replace.matches[i]});
+            console.log(`[emoji] replacing ${toReplace}!`);
+            let modifiedMessage = message.content;
+            for (var i = 0; i < toReplace.length; i++) {
+                let r = toReplace[i];
+                modifiedMessage = modifiedMessage.replace(replace.prefix + r, replace.matches[r]);
+            }
+            message.reply(modifiedMessage);
         }
-        message.reply(modifiedMessage);
-    }
 
-    if (!message.content.startsWith(config.prefix)) return;
-    let cmd = message.content.substring(1).trim();
-
-    if (cmds[cmd]) {
-        message.channel.sendMessage(cmds[cmd]);
+        if (!message.content.startsWith(config.prefix)) return;
+    } else {
+        // Must be prepended with the prefix and not a bot (for using with bot account)
+        if (message.author.bot) return;
+        if (message.author.id !== config.ownerID) return;
+        if (!message.content.startsWith(config.prefix)) return;
+        let cmd = message.content.substring(1).trim();
+        if (cmds[cmd]) {
+            message.channel.sendMessage(cmds[cmd]);
+        }
     }
 });
 
